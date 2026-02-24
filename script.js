fe9c618d10970e5745e9fd037fdf68cbe2d5ee25
singhgobindsingh19@gmail.com <singhgobingsingh19@gmail.com>
2026-02-23 22:22:02 -0500

    First save

---
+++ script.js
@@ -0,0 +1,40 @@
+const informationButton = document.getElementById("Button1")
+const misuseAndEffectsButton = document.getElementById("Button2")
+const jobReplacementButton = document.getElementById("Button3")
+const aiAndClimateButton = document.getElementById("Button4")
+const misinformationAndFraudButton = document.getElementById("Button5")
+const gamesAndVideosButton = document.getElementById("Button6")
+const sourcesButton = document.getElementById("Button7")
+const hoverSound = document.getElementById("hoverSound")
+
+function hoverSoundFunction(){
+    hoverSound.currentTime = 0;
+    hoverSound.play();
+}
+informationButton.addEventListener('mouseenter', () => {
+    hoverSoundFunction();
+})
+misuseAndEffectsButton.addEventListener('mouseenter', () => {
+    hoverSoundFunction();
+})
+jobReplacementButton.addEventListener('mouseenter', () => {
+    hoverSoundFunction();
+})
+aiAndClimateButton.addEventListener('mouseenter', () => {
+    hoverSoundFunction();
+})
+sourcesButton.addEventListener('mouseenter', () => {
+    hoverSoundFunction();
+})
+misinformationAndFraudButton.addEventListener('mouseenter', () => {
+    hoverSoundFunction();
+})
+gamesAndVideosButton.addEventListener('mouseenter', () => {
+    hoverSoundFunction();
+})
+
+
+function clickSoundPlay(){
+    let sound = document.getElementById('clickSound');
+    sound.play();
+}
